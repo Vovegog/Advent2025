13aa2d07a7e57d2dfa2eff8826d6b9a8054dbf80
Vovegog <daniel.viggen@hotmail.com>
2025-12-03 01:51:20 +0100

    Day 1 part 1 complete

--- Puzzle solves/day1_1.py
+++ Puzzle solves/day1_1.py
@@ -2,15 +2,29 @@ def password_finder():
     from pathlib import Path
 
     cwd = Path(__file__)
-    print(f"Current directory: {cwd}")
-
     puzzle_input = (cwd.parent.parent/"Puzzle Inputs"/"day1.txt")
-    print(f"Puzzle input path: {puzzle_input}")
-
 
     with open(puzzle_input, "r") as file:
         lines = file.readlines()
 
+
+    zero_count: int = 0
+    dial: int = 50
+
+    for line in lines:
+        if line[0] == "L":
+            dial -= int(line[1:])
+            while dial < 0:
+                dial += 100
+        elif line[0] == "R":
+            dial += int(line[1:])
+            while dial > 99:
+                dial -= 100
+
+        if dial == 0:
+            zero_count += 1
+
     print(f"Total number of lines: {len(lines)}")
+    print(f"Number of times dial hit zero: {zero_count}")
 
 password_finder()
