86d50e935372639939653f59573298169d11a802
Vovegog <daniel.viggen@hotmail.com>
2025-12-03 03:32:31 +0100

    Got 1 off on part 2. Sleep and try again

--- Puzzle solves/day1_2.py
+++ Puzzle solves/day1_2.py
@@ -9,10 +9,7 @@ def password_finder(dial: int = 50, zero_count: int = 0, dial_nums: int = 100):
 
     for line in lines:
         if line[0] == "L":
-            
-
-        if dial == 0:
-            zero_count += 1
+            pass
 
     print(f"Total number of lines: {len(lines)}")
     print(f"Number of times dial hit zero: {zero_count}")
